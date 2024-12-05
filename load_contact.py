23d99ed753ffc5a76e9867a3abbb2e0d3df6307d
Md Sabbir Hossain <sabbir.softengr@gmail.com>
2024-12-05 19:37:57 +0600

    Update load_contact.py

--- load_contact.py
+++ load_contact.py
@@ -1,1 +1,17 @@
+import csv
 
+filename = "all_contacts.csv"
+
+def load_contacts_from_file(filename):
+    all_contacts = []
+
+    try:
+        with open(filename, mode='r', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                all_contacts.append(row)
+        print("Contacts loaded successfully.")
+
+    except FileNotFoundError:
+        print("No previous contacts found. Starting fresh.")
+    return all_contacts
