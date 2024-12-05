551eb355e90df8b8d07315c5c64a9d5268e91068
Md Sabbir Hossain <sabbir.softengr@gmail.com>
2024-12-05 13:25:28 +0600

    Update add_contact.py

--- add_contact.py
+++ add_contact.py
@@ -1,15 +1,52 @@
 from save_contact import save_contact
 
 def add_contact(all_contacts):
-    name = input("Enter Contact Name: ")
-    email = input("Please Enter A Valid Email Address: ")
-    Phone_number = int(input("Please Enter A Valid unique Phone Number: "))
-    address = input("Please Enter Contact Address: ")
-    nationality = input("Enter Contact Nationality: ")
-    religion = input("Enter Contact Religion: ")
+    while True:
+        name = input("Enter Contact Name: ").strip()
+        if name.isalpha(): 
+            break
+        else:
+            print("Name must be write in Letters. Please try again.")
 
-    if Phone_number in all_contacts:
-        print('This Phone number is alreay exist in the system')
+    while True:
+        email = input("Please Enter A Valid Email Address: ").strip()
+        if "@" in email and "." in email:
+            break
+        else:
+            print("Invalid email format. Please include '@' and a domain.")
+
+    while True:
+        phone_number = input("Please Enter A Valid Unique Phone Number: ").strip()
+
+        if phone_number.isdigit() and phone_number not in all_contacts:
+            break
+        elif phone_number in all_contacts:
+            print("This Phone number already exists in the system.")
+        else:
+            print("Phone number should only contain digits. Please try again.")
+
+    while True:
+        address = input("Please Enter Contact Address: ").strip()
+        if address.isalpha():
+            break
+        else:
+            print("Address must be write in Alphabetically")
+
+
+    while True:
+        nationality = input("Enter Contact Nationality: ").strip()
+        if nationality.isalpha():
+            break
+        else:
+            print("Nationality Name must be write in Letters. Please try again.")
+
+
+    while True:
+        religion = input("Enter Contact Religion: ").strip()
+        if religion.isalpha():
+            break
+        else:
+            print("Religion Name must be write in Letters. Please try again.")
 
     else:
         all_contacts.append({
