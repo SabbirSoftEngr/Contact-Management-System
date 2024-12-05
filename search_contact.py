dca5371a55741390325daa268ff509f81c595390
Md Sabbir Hossain <sabbir.softengr@gmail.com>
2024-12-05 19:38:28 +0600

    Update search_contact.py

--- search_contact.py
+++ search_contact.py
@@ -1,5 +1,5 @@
 def SearchContact(all_contacts):
-    Phone_number = int(input("Enter a phone number to search: "))
+    Phone_number = input("Enter a phone number to search: ").strip()
     found = False
     for contact_search in all_contacts:
         if contact_search["Phone_Number"] == Phone_number:
@@ -7,5 +7,5 @@ def SearchContact(all_contacts):
             print(contact_search)
             found = True
             break
-    if not found:
-        print("No contact found with the given phone number.")
+        else:
+            print("No contact found with the given phone number.")
