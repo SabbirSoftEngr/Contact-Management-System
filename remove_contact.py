bc18c6cfe93fc6b3787a51fa966a660579df2b94
Md Sabbir Hossain <sabbir.softengr@gmail.com>
2024-12-05 10:57:10 +0600

    Update remove_contact.py

--- remove_contact.py
+++ remove_contact.py
@@ -1,3 +1,20 @@
-
+def remove_contact(all_contacts):
+    if len(all_contacts) == 0:
+        print("No contacts to remove.")
+    else:
+        try:
+            phone_number_to_remove = int(input("Enter phone number to remove: "))
+            found = False
+            for contact in all_contacts:
+                if contact["Phone_Number"] == phone_number_to_remove:
+                    all_contacts.remove(contact)
+                    found = True
+                    print("Contact removed successfully!")
+                    break
+            if not found:
+                print("Contact not found in the list.")
+        except ValueError:
+            print("Invalid phone number. Please enter a Valid Phone Number.")
+    return all_contacts
 
 
